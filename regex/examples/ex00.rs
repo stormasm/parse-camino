7873e94dd6e6af97d3d21b2fe9c3c8625c05d625
Michael Angerman <stormasm@gmail.com>
2021-12-27 18:59:40 -0800

    so we are now grabbing the day and date for each day

--- regex/examples/ex00.rs
+++ regex/examples/ex00.rs
@@ -22,7 +22,8 @@ fn regex_t1(s1: &str) -> bool {
 
     //    let re = Regex::new(r"^(?:Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)*$").unwrap();
 
-    let re = Regex::new(r"\W*(Sunday)\W*").unwrap();
+    let re =
+        Regex::new(r"\W*(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)\W*").unwrap();
     let val = re.is_match(s1);
     //println!("{:?}", val);
     return val;
