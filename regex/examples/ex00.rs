90689690506e0c1bffbe716795edd75059e97463
Michael Angerman <stormasm@gmail.com>
2021-12-27 19:30:17 -0800

    working well and documented

--- regex/examples/ex00.rs
+++ regex/examples/ex00.rs
@@ -4,12 +4,12 @@ use std::io::prelude::*;
 use std::io::{self, BufReader};
 
 fn main() -> io::Result<()> {
-    let f = File::open("/j/tmp99/parse-camino-data/t1.txt")?;
+    let f = File::open("/j/tmp99/parse-camino-data/t3.txt")?;
     let f = BufReader::new(f);
 
     for line in f.lines() {
         let x = line.unwrap();
-        let b = regex_t1(&x.clone());
+        let b = regex_day_and_date(&x.clone());
         if b {
             println!("{:?}", x);
         }
@@ -17,14 +17,17 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn regex_t1(s1: &str) -> bool {
-    //    ^((?:mon|tue|wed|thu|fri|sat|sun)(?:, )?)(?1)*$
+/*
+This matches on a day name followed by a space followed by a two digit number
+Key point being is that if there is a day name somewhere else in the text it
+will not match on that particular day name unless by happenstance it has
+a space followed by a two digit number after it and if that case ever happens
+I can change that two digit number and write it out instead.
+So we should never get a match inside the text entry.
+*/
 
-    //    let re = Regex::new(r"^(?:Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)*$").unwrap();
-
+fn regex_day_and_date(s1: &str) -> bool {
     let re =
-        Regex::new(r"(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)\s\d{2}\b").unwrap();
-    let val = re.is_match(s1);
-    //println!("{:?}", val);
-    return val;
+        Regex::new(r"(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)\s\d{2}").unwrap();
+    re.is_match(s1)
 }
